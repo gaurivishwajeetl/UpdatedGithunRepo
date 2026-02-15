48a1bd305721da81badc3e9c61bf6f8a1f1a1b5b
Gauri <lokhandegauri2014@gmail.com>
2026-02-15 22:01:18 +0530

    Framework Created by TL

---
+++ src/main/java/APIGithub/App.java
@@ -0,0 +1,13 @@
+package APIGithub;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
